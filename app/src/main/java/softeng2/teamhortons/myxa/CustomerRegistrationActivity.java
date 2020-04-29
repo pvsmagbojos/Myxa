a8c28e8b105407dfa77702ca5de377d790edf23d
IamJayce <johncarlo.jumawan212@gmail.com>
2020-04-29 11:13:04 +0800

    Customer Registration
    Signed-off-by: IamJayce <johncarlo.jumawan212@gmail.com>

---
+++ app/src/main/java/softeng2/teamhortons/myxa/CustomerRegistrationActivity.java
@@ -0,0 +1,23 @@
+package softeng2.teamhortons.myxa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class CustomerRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_registration);
+
+        Spinner genderSpinner = findViewById(R.id.spinnerGender);
+
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<String>(CustomerRegistrationActivity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.gender));
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderAdapter);
+    }
+}
