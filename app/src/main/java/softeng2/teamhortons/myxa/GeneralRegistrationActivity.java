d77975b15bd51b76c466c7a9aa7556527fa3c207
IamJayce <johncarlo.jumawan212@gmail.com>
2020-04-29 21:51:38 +0800

    General Registration
    Signed-off-by: IamJayce <johncarlo.jumawan212@gmail.com>

--- app/src/main/java/softeng2/teamhortons/myxa/GeneralRegistrationActivity.java
+++ app/src/main/java/softeng2/teamhortons/myxa/GeneralRegistrationActivity.java
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class GeneralRegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_general_select_registration_type);
 
     }
 }
