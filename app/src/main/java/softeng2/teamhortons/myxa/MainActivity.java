ca003a6db0c8d218d7ff63f0115fe40b1c140a44
Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>
2020-04-29 11:55:05 +0800

    Added timer as placeholder before redirecting to login activity
    Signed-off-by: Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>

--- app/src/main/java/softeng2/teamhortons/myxa/MainActivity.java
+++ app/src/main/java/softeng2/teamhortons/myxa/MainActivity.java
@@ -2,7 +2,9 @@ package softeng2.teamhortons.myxa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CountDownTimer countDownTimer = new CountDownTimer(5000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        }.start();
     }
 }
