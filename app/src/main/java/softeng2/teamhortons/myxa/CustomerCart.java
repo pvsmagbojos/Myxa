eabffd644e30146d30df9b4943ccc2462d47a498
Sebastian Ceblano <201801464@iacademy.edu.ph>
2020-05-01 14:02:35 +0800

    Customer View/Edit Cart

---
+++ app/src/main/java/softeng2/teamhortons/myxa/CustomerCart.java
@@ -0,0 +1,14 @@
+package softeng2.teamhortons.myxa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerCart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_cart);
+    }
+}
