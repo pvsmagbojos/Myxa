502c588ceaa3b3d4cf29b97d2e7899910678b5d0
sumstudent <201801481@gmail.com>
2020-05-03 02:25:42 +0800

    Created base file for bottomNavigation Push requested: JC
    Signed-off-by: sumstudent <201801481@gmail.com>

---
+++ app/src/main/java/softeng2/teamhortons/myxa/ui/homepage/HomePageActivity.java
@@ -0,0 +1,18 @@
+package softeng2.teamhortons.myxa.ui.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import softeng2.teamhortons.myxa.R;
+
+public class HomePageActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
