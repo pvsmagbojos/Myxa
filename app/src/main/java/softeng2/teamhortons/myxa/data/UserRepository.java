2fd6c6d9f7164b5c0f729b111196447a76fc3c09
Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>
2020-05-07 01:07:37 +0800

    Moved recordToDatabase method to UserRepository class
    Signed-off-by: Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>

--- app/src/main/java/softeng2/teamhortons/myxa/data/UserRepository.java
+++ app/src/main/java/softeng2/teamhortons/myxa/data/UserRepository.java
@@ -1,6 +1,16 @@
 package softeng2.teamhortons.myxa.data;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Class that requests authentication and user information from the Firebase database and
@@ -23,5 +33,31 @@ public class UserRepository {
             instance = new UserRepository();
         }
         return instance;
+    }
+
+    public void recordToDatabase(final String userId, String fName, String lName, boolean isMale, int age, String email){
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("email", email);
+        user.put("first", fName);
+        user.put("last", lName);
+        user.put("age", age);
+        user.put("isMale", isMale);
+
+        // Add a new document with a generated ID
+        db.collection("users").document(userId).set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("success", "Database connection Success at " + db.document(userId));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("error", "Database connection Error", e);
+            }
+        });
     }
 }
