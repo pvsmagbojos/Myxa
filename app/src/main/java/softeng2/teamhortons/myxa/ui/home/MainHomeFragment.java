c6ac152891fa3bd3588cc1150d341ee0a1833afb
IamJayce <johncarlo.jumawan212@gmail.com>
2020-05-06 22:14:35 +0800

    Home Tab layout (6/10)
    Signed-off-by: IamJayce <johncarlo.jumawan212@gmail.com>

--- app/src/main/java/softeng2/teamhortons/myxa/ui/home/MainHomeFragment.java
+++ app/src/main/java/softeng2/teamhortons/myxa/ui/home/MainHomeFragment.java
@@ -1,9 +1,11 @@
 package softeng2.teamhortons.myxa.ui.home;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -18,6 +20,7 @@ import android.widget.FrameLayout;
 import com.google.android.material.tabs.TabLayout;
 
 import softeng2.teamhortons.myxa.R;
+import softeng2.teamhortons.myxa.ViewRecipe;
 
 
 public class MainHomeFragment extends Fragment {
@@ -37,5 +40,20 @@ public class MainHomeFragment extends Fragment {
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_home, container, false);
+    }
+
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final Button viewRecipe = (Button)getView().findViewById(R.id.button30);
+
+        viewRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(getContext(), ViewRecipe.class),
+                        ViewRecipe.REQUEST_CODE);
+            }
+        });
     }
 }
