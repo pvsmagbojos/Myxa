476ffa7503ed309fa4f0a6ef3c368bde2f0ff137
IamJayce <johncarlo.jumawan212@gmail.com>
2020-05-07 19:50:38 +0800

    refactor
    Signed-off-by: IamJayce <johncarlo.jumawan212@gmail.com>

--- app/src/main/java/softeng2/teamhortons/myxa/ui/home/MainHomeFragment.java
+++ app/src/main/java/softeng2/teamhortons/myxa/ui/home/MainHomeFragment.java
@@ -1,23 +1,15 @@
 package softeng2.teamhortons.myxa.ui.home;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
-
-import com.google.android.material.tabs.TabLayout;
 
 import softeng2.teamhortons.myxa.R;
 import softeng2.teamhortons.myxa.ViewRecipe;
@@ -39,21 +31,7 @@ public class MainHomeFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_home, container, false);
+        return inflater.inflate(R.layout.fragment_browse_home, container, false);
     }
 
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        final Button viewRecipe = (Button)getView().findViewById(R.id.button30);
-
-        viewRecipe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivityForResult(
-                        new Intent(getContext(), ViewRecipe.class),
-                        ViewRecipe.REQUEST_CODE);
-            }
-        });
-    }
 }
