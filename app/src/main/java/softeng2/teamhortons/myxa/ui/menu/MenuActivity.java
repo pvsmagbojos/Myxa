1713fdad422122c9b7c6c22409870d862e631c73
Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>
2020-05-11 18:07:32 +0800

    Optimized fragment navigation in menu activity
    Signed-off-by: Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>

--- app/src/main/java/softeng2/teamhortons/myxa/ui/menu/MenuActivity.java
+++ app/src/main/java/softeng2/teamhortons/myxa/ui/menu/MenuActivity.java
@@ -1,77 +1,26 @@
 package softeng2.teamhortons.myxa.ui.menu;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
 
-import softeng2.teamhortons.myxa.R;
-import softeng2.teamhortons.myxa.ui.menu.fragment.ScheduleFragment;
-import softeng2.teamhortons.myxa.ui.menu.fragment.ShowcaseFragment;
-import softeng2.teamhortons.myxa.ui.menu.fragment.HomeFragment;
-import softeng2.teamhortons.myxa.ui.menu.fragment.CartFragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import softeng2.teamhortons.myxa.R;
 
-public class MenuActivity extends AppCompatActivity
-{
-    private BottomNavigationView mainBottomNavigation;
-    private FrameLayout mainFrameLayout;
 
-    private HomeFragment HomeFragment;
-    private ScheduleFragment scheduleFragment;
-    private CartFragment cartFragment;
-
+public class MenuActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        mainFrameLayout = (FrameLayout)findViewById(R.id.frame_main_frameLayout);
-        mainBottomNavigation = (BottomNavigationView)findViewById(R.id.nav_main_bottomNavigation);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        HomeFragment = new HomeFragment();
-        scheduleFragment = new ScheduleFragment();
-        cartFragment = new CartFragment();
-
-        setFragment(HomeFragment);
-
-        mainBottomNavigation.setSelectedItemId(R.id.nav_home);
-
-
-        mainBottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
-        {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item)
-            {
-                switch (item.getItemId())
-                {
-                    case R.id.nav_diet_schedule:
-                        setFragment(scheduleFragment);
-                        return true;
-                    case R.id.nav_home:
-                        setFragment(HomeFragment);
-                        return true;
-                    case R.id.nav_your_cart:
-                        setFragment(cartFragment);
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-        });
-    }
-
-    private void setFragment(Fragment fragment)
-    {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.frame_main_frameLayout,fragment);
-        fragmentTransaction.commit();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
