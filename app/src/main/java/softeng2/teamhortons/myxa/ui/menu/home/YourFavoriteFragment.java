705b2efe8dd4c80f6a009a81eff53bdf5f0f2623
Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>
2020-05-17 19:39:28 +0800

    Added corner radius to recipe items
    Signed-off-by: Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>

--- app/src/main/java/softeng2/teamhortons/myxa/ui/menu/home/YourFavoriteFragment.java
+++ app/src/main/java/softeng2/teamhortons/myxa/ui/menu/home/YourFavoriteFragment.java
@@ -17,7 +17,6 @@ public class YourFavoriteFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
